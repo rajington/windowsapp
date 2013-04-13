13802c0d526ac6072ea19459471f6cd48db35f21
Steven Degutis <sbdegutis@gmail.com>
2013-04-13 00:06:08 -0500

    Initial Commit

---
+++ Windows/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Windows
+//
+//  Created by Steven on 4/13/13.
+//  Copyright (c) 2013 Giant Robot Software. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
