308b24bafb64bb9c044d02072fc62cc095df7efa
Steven Degutis <sbdegutis@gmail.com>
2013-04-15 23:33:33 -0500

    Refactoring ConfigLoader class

--- Windows/SDConfigLoader.m
+++ Windows/SDConfigLoader.m
@@ -53,60 +53,41 @@
 
 - (void) reloadConfig {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (![self tryCoffeescriptConfig] && ![self tryJavascriptConfig]) {
+        if (![self tryDotConfig:YES] && ![self tryDotConfig:NO]) {
             [self reportProblem:@"~/.windowsapp.{coffee,js} doesn't exist"
                            body:@"Make one exist and try again maybe? (If both exist, coffee is chosen.)"];
         }
     });
 }
 
-- (BOOL) tryJavascriptConfig {
-    NSString* path = [@"~/.windowsapp.js" stringByStandardizingPath];
+- (BOOL) tryDotConfig:(BOOL)useCoffeeVersion {
+    NSString* filename = (useCoffeeVersion ? @"~/.windowsapp.coffee" : @"~/.windowsapp.js");
+    
+    NSString* path = [filename stringByStandardizingPath];
     NSString* config = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
     
     if (config == nil)
         return NO;
     
-    NSString* __autoreleasing invalidReason;
-    BOOL validSyntax = [self.jscocoa isSyntaxValid:config error:&invalidReason];
-    
-    if (validSyntax == NO) {
-        [self reportProblem:@"Your config file has bad syntax."
-                       body:invalidReason];
-        return YES;
+    if (useCoffeeVersion) {
+        JSValueRef compileFn = [self.jscocoa evalJSString:@"CoffeeScript.compile"];
+        JSValueRef compiledCode = [self.jscocoa callJSFunction:(JSObjectRef)compileFn withArguments:@[config]];
+        config = [self.jscocoa toObject:compiledCode];
+    }
+    else {
+        NSString* __autoreleasing invalidReason;
+        BOOL validSyntax = [self.jscocoa isSyntaxValid:config error:&invalidReason];
+        
+        if (validSyntax == NO) {
+            [self reportProblem:@"Your config file has bad syntax."
+                           body:invalidReason];
+            return YES;
+        }
     }
     
     [[SDKeyBinder sharedKeyBinder] removeKeyBindings];
     
     [self.jscocoa evalJSString:config];
-    
-    NSArray* failures = [[SDKeyBinder sharedKeyBinder] finalizeNewKeyBindings];
-    
-    if ([failures count] > 0) {
-        [self reportProblem:@"The following hot keys could not be bound:"
-                       body:[failures componentsJoinedByString:@"\n"]];
-    }
-    else {
-        [[SDPopupWindowController sharedPopupWindowController] show:@"Config reloaded."];
-    }
-    
-    return YES;
-}
-
-- (BOOL) tryCoffeescriptConfig {
-    NSString* path = [@"~/.windowsapp.coffee" stringByStandardizingPath];
-    NSString* config = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
-    
-    if (config == nil)
-        return NO;
-    
-    JSValueRef compileFn = [self.jscocoa evalJSString:@"CoffeeScript.compile"];
-    JSValueRef compiledCode = [self.jscocoa callJSFunction:(JSObjectRef)compileFn withArguments:@[config]];
-    NSString* compiledCodeStr = [self.jscocoa toObject:compiledCode];
-    
-    [[SDKeyBinder sharedKeyBinder] removeKeyBindings];
-    
-    [self.jscocoa evalJSString:compiledCodeStr];
     
     NSArray* failures = [[SDKeyBinder sharedKeyBinder] finalizeNewKeyBindings];
     
