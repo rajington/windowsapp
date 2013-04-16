65a3fe06c73d7a8b36f235c42e41572ab2557cd2
Steven Degutis <sbdegutis@gmail.com>
2013-04-16 10:07:14 -0500

    fix brew sha

--- windows-app.rb
+++ windows-app.rb
@@ -4,7 +4,7 @@ class WindowsApp < Formula
   homepage 'https://github.com/sdegutis/windows.app'
   url 'https://github.com/sdegutis/windows.app/archive/2.0.2.zip'
   version '2.0.2'
-  sha1 'bdd9a5a56a70afd2f0243741aeb47c50fd4facff'
+  sha1 'a8e94d67ce24b7926652a5fbd7180ba3b444958b'
   head 'https://github.com/sdegutis/Windows.git'
 
   depends_on :xcode
